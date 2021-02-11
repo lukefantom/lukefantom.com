1ce2d9afa906fff21f69c4eb35959acd155f1afe
Luke Fantom <luke.fantom@gmail.com>
2021-02-11 17:59:29 +0000

    cleanup

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,10 @@
 import "../styles/global.css";
 import { ThemeProvider } from "next-themes";
-import { ScrollProvider } from "../Context/scrollContext";
 
 export default function App({ Component, pageProps }) {
   return (
     <ThemeProvider attribute="class">
-      <ScrollProvider>
-        <Component {...pageProps} />
-      </ScrollProvider>
+      <Component {...pageProps} />
     </ThemeProvider>
   );
 }
